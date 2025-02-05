111845b5040ed3a3a1352c63929cc6b83884cfca
“Emanuele <emanuelegardina@gmil.com>
2025-02-05 10:12:52 +0100

    modifica

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -1,11 +1,12 @@
+import { Container } from "react-bootstrap";
 import Alert from "react-bootstrap/Alert";
 
 const Welcome = () => {
   return (
-    <container className="my-3">
+    <Container className="my-3">
       <Alert variant="success">Benvenuti nel nostro EPICO negozio di libri!</Alert>
       <h2 className="text-center">BOOKSHOP</h2>
-    </container>
+    </Container>
   );
 };
 export default Welcome;
